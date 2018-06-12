3a15855d00c417110b6f7235dc05571e16cedbcd
Nash Pherson <napherso@microsoft.com>
2018-06-12 14:39:50 -0500

    First attempt at fixing the 'Server? 2008' character. And notes.

--- Get-ADComputerInfo.ps1
+++ Get-ADComputerInfo.ps1
@@ -1,7 +1,31 @@
-# Requires the following component is installed:
-# Remote Server Administration Tools > Active Directory Module for Powershell
-# Nash Pherson 2017-03-15
-# 
+<#
+    .SYNOPSIS
+        Used to collect key information about Computer objects in Active Directory.
+
+    .DESCRIPTION
+        Use the Get-ADComputerinfo.ps1 script to output details about all the Computer objects in Active Directory to a CSV file.    
+
+        The script will attempt to query all domains in the forest that you are connected to.
+
+        Nope, this script doesn't have forest/domain parameters built it... feel free to make contributions on GitHub.
+
+        You can schedule this script to run as a scheduled task with any domain user with 'read' rights in the directory.
+        SCHTASKS /Create /TN "ConfigMgr Start-AICategorization" /SC Daily /TR "POWERSHELL.EXE -ExecutionPolicy Bypass -File C:\Scripts\Get-AdComputerInfo.ps1" /RU domain\AnyUserWithReadRights /RP *
+
+    .NOTES
+        Author  : Nash Pherson
+        Email   : napherso@microsoft.com
+        Twitter : @KidMytsic  https://twitter.com/kidmystic        
+        
+    .LINK
+        https://gallery.technet.microsoft.com/Get-ADComputerInfops1-b5b3e656
+    
+    .LINK
+        https://github.com/npherson/Get-ADComputerInfo.ps1
+
+#>
+
+
 
 # Set the name of the output CSV file...
 $output = "CompList-$(get-date -f yyyy-MM-dd-hh.mm.ss).csv"
@@ -35,6 +59,9 @@ Foreach ($i in $Domains)
         select-object Name,@{Name="lastLogonTimestamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}},Enabled,operatingSystem,operatingSystemVersion,operatingSystemServicePack,distinguishedName,Description,@{Name="Domain"; Expression={$i}}
         $AllComputers += $Computers
     }
+
+# Strip out the special character from Server 2008 objects...
+$AllComputers = $Allcomputers -replace "Server?","Server"
 
 # Write computer object data to CSV file...
 $AllComputers | Export-CSV $output -NoClobber -NoTypeInformation
