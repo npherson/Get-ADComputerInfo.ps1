59e007ce6eda8e0f4eca7042716dc0c3e9a692d1
Nash Pherson <napherso@microsoft.com>
2018-06-12 14:00:13 -0500

    Enhanced AD Module checking for Win10 Build 17682 and above.

--- Get-ADComputerInfo.ps1
+++ Get-ADComputerInfo.ps1
@@ -6,8 +6,20 @@
 # Set the name of the output CSV file...
 $output = "CompList-$(get-date -f yyyy-MM-dd-hh.mm.ss).csv"
 
-# Check to see if ActiveDirectory module is installed...
-If (!(Get-Module -ListAvailable -Name ActiveDirectory)) {Write-Warning "To use this script, you must install the Active Directory Module for PowerShell.";Break}
+# Check to see if ActiveDirectory Module is installed...
+If (!(Get-Module -ListAvailable -Name ActiveDirectory))
+    {
+        # No AD Module installed... End user needs to install RSAT first
+        If ([System.Environment]::OSVersion.Version.Build -lt 17682) {
+            Write-Warning "To use this script, you must install the Active Directory Module for PowerShell (part of RSAT).";Break
+        }
+ 
+        # No AD Module installed... Try to install it via Features on Demand (Win 10 Build 17682 and above)
+        Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+        If (!(Get-Module -ListAvailable -Name ActiveDirectory)) {
+            Write-Warning "To use this script, you must install the Active Directory Module for PowerShell. Could not install RSAT for you.";Break
+        }
+    }   
 
 # Load the ActiveDirctory module...
 If (!(Get-Module ActiveDirectory)) {Import-Module ActiveDirectory}
